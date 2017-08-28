13164ddb98cff59789b433bdb30c44c87f2e29e6
arfro <aerfromenes@gmail.com>
2017-08-28 18:09:18 +0100

    added test folder with JUnit test

---
+++ test/MyLinkedList_JUnit.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class MyLinkedList_JUnit {
+    @Test
+    public void positive_addTwoElements() {
+      MyLinkedList list = new MyLinkedList();
+      list.insertToEnd(new Node("last"));
+      list.insertToFront(new Node("first"));
+      assertEquals("first last", list;)
+    }
+    
+
+}
