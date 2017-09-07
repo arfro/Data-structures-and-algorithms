b139070044f1c8ff6bcacdbf250a9a3832dc7619
arfro <aerfromenes@gmail.com>
2017-09-07 14:34:27 +0100

    Adding SelectionSort class tests

---
+++ sorting_algorithms/test/SelectionSort_JUnit.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SelectionSort_JUnit{
+	
+	@Test
+	public void SelectionSort_sort_PositiveTest(){
+		int[] array = new int[50];
+		SelectionSort.fillArray(array);
+		assertEquals(Arrays.sort(array), SelectionSort.selectionsort(array));
+	}
+	
+	@Test
+	public void SelectionSort_sort_NoElements(){
+		int[] array = new int[0];
+		assertEquals(array, SelectionSort.selectionsort(array));
+	}
+	
+	@Test
+	public void SelectionSort_sort_NoElements(){
+		int[] array = new int[1];
+		SelectionSort.fillArray(array);
+		assertEquals(array, SelectionSort.selectionsort(array));
+	}
+	
+}
